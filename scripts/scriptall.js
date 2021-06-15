a0f7ea7a09bf320aee46a5e61512a7875ef4e590
Lisseth Arce <lisseth.arce96@gmail.com>
2021-06-14 19:28:58 -0600

    updates

--- scripts/scriptall.js
+++ scripts/scriptall.js
@@ -1,55 +1,54 @@
-function vermas(id){
-    if(id=="mas"){
-    document.getElementById("desplegar").style.display="block";   
-    document.getElementById("mas").style.display="none"; 
+function vermas(id) {
+    if (id == "mas") {
+        document.getElementById("desplegar").style.display = "block";
+        document.getElementById("mas").style.display = "none";
     }
-    else{
-    document.getElementById("desplegar").style.display="none";
-    document.getElementById("mas").style.display="inline";
+    else {
+        document.getElementById("desplegar").style.display = "none";
+        document.getElementById("mas").style.display = "inline";
     }
-    }
+}
 
-    function showCategory(type) {
-        var mascotasElement= document.getElementById("mascotas");
-        var paseosElement= document.getElementById("paseos");
-        var hobbiesElement= document.getElementById("hobbies");
-   
-     var mascotasBtnElement= document.getElementById("mascotas-btn");
-     var paseosBtnElement = document.getElementById("paseos-btn");
-     var hobbiesBtnElement = document.getElementById("hobbies-btn");
-   
+function showCategory(type) {
+    var mascotasElement = document.getElementById("mascotas");
+    var paseosElement = document.getElementById("paseos");
+    var hobbiesElement = document.getElementById("hobbies");
+
+    var mascotasBtnElement = document.getElementById("mascotas-btn");
+    var paseosBtnElement = document.getElementById("paseos-btn");
+    var hobbiesBtnElement = document.getElementById("hobbies-btn");
+
     switch (type) {
         case "mascotas":
-            paseosElement.style.display ="none";
+            paseosElement.style.display = "none";
             hobbiesElement.style.display = "none";
             mascotasElement.style.display = "block";
-   
+
             paseosBtnElement.style.backgroundColor = "#005bbb";
             hobbiesBtnElement.style.backgroundColor = "#005bbb;";
             mascotasBtnElement.style.backgroundColor = "#ffff00";
-   
+
             break;
-            case "paseos":
-           
+        case "paseos":
+
             hobbiesElement.style.display = "none";
             mascotasElement.style.display = "none";
             paseosElement.style.display = "block";
-   
-            mascotasBtnElement.style. backgroundColor ="#005bbb";
-            hobbiesBtnElement.style. backgroundColor = "#005bbb";
-            paseosBtnElement.style. backgroundColor = "#ffff00";
-            
-           break;
-    default:
-        mascotasElement.style.display ="none";
-        paseosElement.style.display = "none";
-        hobbiesElement.style.display = "block";
-   
-        paseosBtnElement.style.backgroundColor ="#005bbb";
-        mascotasBtnElement.style.backgroundColor = "#005bbb";;
-        hobbiesBtnElement.style.backgroundColor = "#ffff00";
-        break;
-   
-       }
-   }
-   
+
+            mascotasBtnElement.style.backgroundColor = "#005bbb";
+            hobbiesBtnElement.style.backgroundColor = "#005bbb";
+            paseosBtnElement.style.backgroundColor = "#ffff00";
+
+            break;
+        default:
+            mascotasElement.style.display = "none";
+            paseosElement.style.display = "none";
+            hobbiesElement.style.display = "block";
+
+            paseosBtnElement.style.backgroundColor = "#005bbb";
+            mascotasBtnElement.style.backgroundColor = "#005bbb";;
+            hobbiesBtnElement.style.backgroundColor = "#ffff00";
+            break;
+
+    }
+}
